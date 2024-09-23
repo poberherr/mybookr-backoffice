76762e7725dc3e9ac5b47494f2c9ab9988c6a35d
Benedikt Rötsch <benedikt@hashbite.net>
2024-09-23 17:13:41 +0200

    feat: pass clerk auth token down to our graphql api

--- src/components/AdminApp.tsx
+++ src/components/AdminApp.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useMemo, useState } from "react";
 import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import buildGraphQLProvider, {
   buildQueryFactory,
@@ -16,6 +17,7 @@ import {
 
 import buildGqlQuery from "@/graphql/buildGqlQuery";
 import getResponseParser from "@/graphql/getResponseParser";
+
 import { ExperienceList } from "./lists/ExperienceList";
 import { CategoryList } from "./lists/CategoryList";
 import { BookingList } from "./lists/BookingList";
@@ -24,6 +26,7 @@ import { MediaList } from "./lists/MediaList";
 import { PaymentList } from "./lists/PaymentList";
 import { BookingEdit } from "./edits/BookingEdit";
 import { LocationEdit } from "./edits/LocationEdit";
+import { SignInButton, useAuth } from "@clerk/nextjs";
 
 const buildQuery = buildQueryFactory(
   undefined,
@@ -31,84 +34,108 @@ const buildQuery = buildQueryFactory(
   getResponseParser,
 );
 
-const dataProvider = buildGraphQLProvider({
-  clientOptions: {
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-    headers: {
-      Authorization: process.env.NEXT_PUBLIC_GRAPHQL_AUTH_HEADER || "",
-    },
-  },
-  buildQuery,
-  introspection: {
-    operationNames: {
-      [GET_LIST]: (resource) => `${resource.name}s`.toLowerCase(),
-      [GET_ONE]: (resource) => resource.name.toLowerCase(),
-      [GET_MANY]: (resource) => `${resource.name}s`.toLowerCase(),
-      [GET_MANY_REFERENCE]: (resource) => `${resource.name}s`.toLowerCase(),
-      [CREATE]: (resource) => `create${resource.name}`,
-      [UPDATE]: (resource) => `update${resource.name}`,
-      [DELETE]: (resource) => `delete${resource.name}`,
-    },
-  },
-});
+const AdminApp = () => {
+  const {getToken} = useAuth();
 
-const AdminApp = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource
-      name="Experience"
-      list={ExperienceList}
-      edit={EditGuesser}
-      recordRepresentation="title"
-    />
-    <Resource
-      name="Booking"
-      list={BookingList}
-      edit={BookingEdit}
-      recordRepresentation="id"
-    />
-    <Resource
-      name="Activity"
-      list={ActivityList}
-      edit={EditGuesser}
-      recordRepresentation="title"
-    />
-    <Resource
-      name="Category"
-      list={CategoryList}
-      edit={EditGuesser}
-      recordRepresentation="name"
-    />
-    <Resource
-      name="Location"
-      list={ListGuesser}
-      edit={LocationEdit}
-      recordRepresentation="addressLineOne"
-    />
-    <Resource
-      name="Media"
-      list={MediaList}
-      edit={EditGuesser}
-      recordRepresentation="title"
-    />
-    <Resource
-      name="Operator"
-      list={ListGuesser}
-      edit={EditGuesser}
-      recordRepresentation="name"
-    />
-    <Resource
-      name="Payment"
-      list={PaymentList}
-      edit={EditGuesser}
-      recordRepresentation="name"
-    />
-    <Resource
-      name="User"
-      list={ListGuesser}
-      edit={EditGuesser}
-      recordRepresentation="name"
-    />
-  </Admin>
-);
+    const [token, setToken] = useState<string | null>(null);
+
+    useEffect(() => {
+      const fetchToken = async () => {
+        const fetchedToken = await getToken();
+        setToken(fetchedToken);
+      };
+
+      fetchToken();
+    }, [getToken]);
+
+  const dataProvider = useMemo(() => {
+    if (!token) {
+      return null;
+    }
+    return buildGraphQLProvider({
+      clientOptions: {
+        // link: createAuthLink().concat(httpLink),
+        uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+      buildQuery,
+      introspection: {
+        operationNames: {
+          [GET_LIST]: (resource) => `${resource.name}s`.toLowerCase(),
+          [GET_ONE]: (resource) => resource.name.toLowerCase(),
+          [GET_MANY]: (resource) => `${resource.name}s`.toLowerCase(),
+          [GET_MANY_REFERENCE]: (resource) => `${resource.name}s`.toLowerCase(),
+          [CREATE]: (resource) => `create${resource.name}`,
+          [UPDATE]: (resource) => `update${resource.name}`,
+          [DELETE]: (resource) => `delete${resource.name}`,
+        },
+      },
+    });
+  }, [token]);
+
+  if (dataProvider === null) {
+    return <SignInButton />;
+  }
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="Experience"
+        list={ExperienceList}
+        edit={EditGuesser}
+        recordRepresentation="title"
+      />
+      <Resource
+        name="Booking"
+        list={BookingList}
+        edit={BookingEdit}
+        recordRepresentation="id"
+      />
+      <Resource
+        name="Activity"
+        list={ActivityList}
+        edit={EditGuesser}
+        recordRepresentation="title"
+      />
+      <Resource
+        name="Category"
+        list={CategoryList}
+        edit={EditGuesser}
+        recordRepresentation="name"
+      />
+      <Resource
+        name="Location"
+        list={ListGuesser}
+        edit={LocationEdit}
+        recordRepresentation="addressLineOne"
+      />
+      <Resource
+        name="Media"
+        list={MediaList}
+        edit={EditGuesser}
+        recordRepresentation="title"
+      />
+      <Resource
+        name="Operator"
+        list={ListGuesser}
+        edit={EditGuesser}
+        recordRepresentation="name"
+      />
+      <Resource
+        name="Payment"
+        list={PaymentList}
+        edit={EditGuesser}
+        recordRepresentation="name"
+      />
+      <Resource
+        name="User"
+        list={ListGuesser}
+        edit={EditGuesser}
+        recordRepresentation="name"
+      />
+    </Admin>
+  );
+};
 
 export default AdminApp;
