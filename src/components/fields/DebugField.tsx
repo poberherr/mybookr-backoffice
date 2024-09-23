8ea47cbfa3abe4da7f7a8d06afff7b9bb5fb2a41
Benedikt Rötsch <benedikt@hashbite.net>
2024-09-23 12:40:56 +0200

    feat: introduce debug field

---
+++ src/components/fields/DebugField.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+import { useRecordContext } from "react-admin";
+
+const DebugField = () => {
+  const record = useRecordContext();
+  return (
+    <pre>
+      <code>{JSON.stringify(record, null, 2)}</code>
+    </pre>
+  );
+};
+
+export default DebugField;
