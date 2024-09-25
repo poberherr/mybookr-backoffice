ff19fd19444e16cc1946be249cb6d2dcd512dfaf
Benedikt Rötsch <benedikt@hashbite.net>
2024-09-25 17:19:57 +0200

    feat: add more Matomo widgets to dashboard as we now can embed iframes

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,28 +1,62 @@
-import { Stack } from "@mui/material";
+import { Grid } from "@mui/material";
 
 const Dashboard = () => (
-  <Stack sx={{ marginTop: "1rem", width: "100%", gap: "2rem" }} direction={"row"}>
-    <Stack>
+  <Grid container spacing={6} sx={{ marginTop: "1rem", paddingX: "2rem" }}>
+    <Grid item xs={6}>
       <h2>mybookr.io</h2>
-      <h3>Statistics:</h3>
+
+      <iframe
+        width="100%"
+        height="205"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=Live&actionToWidgetize=getSimpleLastVisitCount&idSite=21&period=day&date=today"
+      />
+
+      <iframe
+        width="100%"
+        height="750"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=21&period=day&date=today"
+      />
       <iframe
         width="100%"
         height="350"
-        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&containerId=Goal_1&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=23&period=day&date=today"
-        scrolling="yes"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountryMap&actionToWidgetize=visitorMap&idSite=21&period=day&date=today"
       />
-    </Stack>
-    <Stack>
+
+      <h3>Conversions:</h3>
+      <iframe
+        width="100%"
+        height="380"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&containerId=Goal_1&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=21&period=day&date=today"
+      />
+    </Grid>
+    <Grid item xs={6}>
       <h2>Eco Dive Bali</h2>
-      <h3>Statistics:</h3>
+
+      <iframe
+        width="100%"
+        height="205"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=Live&actionToWidgetize=getSimpleLastVisitCount&idSite=23&period=day&date=today"
+      />
+
+      <iframe
+        width="100%"
+        height="750"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=23&period=day&date=today"
+      />
       <iframe
         width="100%"
         height="350"
-        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&containerId=Goal_1&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=23&period=day&date=today"
-        scrolling="yes"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountryMap&actionToWidgetize=visitorMap&idSite=23&period=day&date=today"
       />
-    </Stack>
-  </Stack>
+
+      <h3>Conversions:</h3>
+      <iframe
+        width="100%"
+        height="380"
+        src="https://statistics.hashbite.net/index.php?module=Widgetize&action=iframe&containerId=Goal_1&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=23&period=day&date=today"
+      />
+    </Grid>
+  </Grid>
 );
 
 export default Dashboard;
