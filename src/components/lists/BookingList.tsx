57ca1204e9d9aa5454b2b683b48d19d53d502da1
Benedikt Rötsch <benedikt@hashbite.net>
2024-09-25 14:33:06 +0200

    fix: improve booking list columns to be more useful

--- src/components/lists/BookingList.tsx
+++ src/components/lists/BookingList.tsx
@@ -1,26 +1,41 @@
 import {
-    List,
-    Datagrid,
-    TextField,
-    ReferenceField,
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  WrapperField,
 } from "react-admin";
+import { Stack } from "@mui/material";
 
 import GlobalIdTextField from "../fields/GlobalIdTextField";
 import BookingStatusField from "../fields/BookingStatusField";
 import PriceField from "../fields/PriceField";
+
 export const BookingList = () => (
-    <List>
-        <Datagrid>
-            <GlobalIdTextField source="id" />
-            <ReferenceField source="activity.id" reference="Activity" label="Activity Title">
-                <TextField source="title" />
-            </ReferenceField>
-            <TextField source="referenceCode" label="Booking Reference Code" />
-            <BookingStatusField source="status" />
-            <ReferenceField source="booker.id" reference="User" label="Booking User">
-                <TextField source="name" />
-            </ReferenceField>
-            <PriceField source="totalCost" />
-        </Datagrid>
-    </List>
+  <List>
+    <Datagrid>
+      <GlobalIdTextField source="id" />
+      <BookingStatusField source="status" />
+      <WrapperField label="Booker" sortBy="name">
+        <Stack>
+          <TextField source="name" />
+          <Stack direction={"column"}>
+            <TextField source="email" />
+            <TextField source="telephone" />
+          </Stack>
+        </Stack>
+      </WrapperField>
+      <TextField source="referenceCode" label="Booking Reference Code" />
+
+      <PriceField source="totalCost" />
+
+      <ReferenceField
+        source="activity.id"
+        reference="Activity"
+        label="Activity Title"
+      >
+        <TextField source="title" />
+      </ReferenceField>
+    </Datagrid>
+  </List>
 );
