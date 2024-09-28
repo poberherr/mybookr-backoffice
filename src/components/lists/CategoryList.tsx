6aef2f0b7e82832c718242d2af336300c05a3da0
Benedikt Rötsch <benedikt@hashbite.net>
2024-09-28 14:55:45 +0200

    feat: add tree and sunburst chart visualization for categories

--- src/components/lists/CategoryList.tsx
+++ src/components/lists/CategoryList.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import AccountTreeIcon from "@mui/icons-material/AccountTree";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ListIcon from "@mui/icons-material/List";
+import { useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 
+import { MouseHandler, ResponsiveSunburst } from "@nivo/sunburst";
+import { NodeMouseEventHandler, ResponsiveTree } from "@nivo/tree";
 import {
   ChipField,
   Datagrid,
@@ -16,7 +19,12 @@ import {
   ReferenceArrayField,
   ReferenceField,
   TextField,
+  useGetList,
 } from "react-admin";
+import { useNavigate } from "react-router-dom";
+
+import { encodeGlobalId } from "@/helpers/global-ids";
+import { Category, TreeNode, useTreeData } from "@/helpers/ltree";
 
 import GlobalIdTextField from "../fields/GlobalIdTextField";
 
@@ -42,10 +50,22 @@ function CustomTabPanel(props: TabPanelProps) {
 }
 
 export const CategoryList = () => {
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const { data } = useGetList<Category>("Category");
+
+  const treeData = useTreeData(data);
+
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
+  };
+
+  const handleNodeClick: NodeMouseEventHandler<TreeNode> = (event) => {
+    navigate(
+      `/Category/${encodeURIComponent(encodeGlobalId("Category", event.data.id))}`,
+    );
   };
 
   return (
@@ -56,6 +76,7 @@ export const CategoryList = () => {
           onChange={handleChange}
           aria-label="basic tabs example"
         >
+          <Tab icon={<ListIcon />} iconPosition="start" label="List" />
           <Tab
             icon={<DonutLargeIcon />}
             iconPosition="start"
@@ -66,20 +87,10 @@ export const CategoryList = () => {
             iconPosition="start"
             label="Tree Chart"
           />
-          <Tab
-            icon={<ListIcon />}
-            iconPosition="start"
-            label="List of Categories"
-          />
         </Tabs>
       </Box>
+
       <CustomTabPanel value={value} index={0}>
-        Item One
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
         <List>
           <Datagrid>
             <GlobalIdTextField source="id" />
@@ -104,6 +115,53 @@ export const CategoryList = () => {
             />
           </Datagrid>
         </List>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <div style={{ height: "69vh" }}>
+          {treeData && (
+            <ResponsiveSunburst
+              data={treeData}
+              value="value"
+              colors={{ scheme: "paired" }}
+              enableArcLabels={true}
+              arcLabel={"data.name"}
+              onClick={handleNodeClick as unknown as MouseHandler<TreeNode>}
+              borderColor={theme.palette.text.primary}
+            />
+          )}
+        </div>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <div style={{ height: "69vh" }}>
+          {treeData && (
+            // @ts-expect-error TS definitions are broken in upstream nivo package.. sooo..
+            <ResponsiveTree
+              data={treeData}
+              label="data.name" // Custom label rendering
+              enableLabel={true}
+              mode="tree"
+              activeNodeSize={24}
+              inactiveNodeSize={12}
+              nodeColor={{ scheme: "tableau10" }}
+              fixNodeColorAtDepth={1}
+              linkThickness={2}
+              activeLinkThickness={6}
+              inactiveLinkThickness={2}
+              linkColor={{
+                from: "target.color",
+                modifiers: [["opacity", 0.4]],
+              }}
+              margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
+              motionConfig="stiff"
+              meshDetectionRadius={80}
+              onNodeClick={handleNodeClick}
+              orientLabel={false}
+              theme={{
+                labels: { text: { fill: theme.palette.text.primary } },
+              }}
+            />
+          )}
+        </div>
       </CustomTabPanel>
     </>
   );
---
+++ src/helpers/ltree.ts
@@ -0,0 +1,80 @@
+import { useMemo } from "react";
+
+import { decodeGlobalId } from "./global-ids";
+
+export interface Category {
+  id: string;
+  path: string;
+  name: string;
+  weight: number;
+  value: number;
+}
+
+export interface TreeNode {
+  id: number;
+  name: string;
+  weight: number;
+  value: number;
+  children: TreeNode[];
+}
+
+interface TreeMap {
+  [key: string]: TreeNode;
+}
+
+function buildTree(categories: Category[]): TreeNode {
+  const tree: TreeMap = {};
+
+  categories.forEach((category) => {
+    const segments = category.path.split(".");
+    let current = tree;
+
+    segments.forEach((segment, index) => {
+      if (!current[segment]) {
+        current[segment] = {
+          id: decodeGlobalId(category.id).id,
+          name: segment,
+          weight: 0, // Temporary, will be overridden if not the last segment
+          children: [],
+          value: 1,
+        };
+      }
+
+      if (index === segments.length - 1) {
+        current[segment].id = decodeGlobalId(category.id).id;
+        current[segment].name = category.name;
+        current[segment].weight = category.weight;
+      }
+
+      current = current[segment].children as unknown as TreeMap;
+    });
+  });
+
+  const convertTreeToArray = (node: TreeNode) => {
+    if (node.children) {
+      node.children = Object.values(node.children).sort(
+        (a, b) => a.weight - b.weight || a.name.localeCompare(b.name),
+      );
+      node.children.forEach(convertTreeToArray);
+    }
+  };
+
+  const rootNodes = Object.values(tree);
+  if (rootNodes.length > 0) {
+    convertTreeToArray(rootNodes[0]);
+    return rootNodes[0];
+  }
+
+  throw new Error("Tree structure could not be built.");
+}
+
+export const useTreeData = (categories: Category[] | undefined) => {
+  const treeData = useMemo(() => {
+    if (!categories) {
+      return undefined;
+    }
+    return buildTree(categories);
+  }, [categories]);
+
+  return treeData;
+};
